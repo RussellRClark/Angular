708759f2af87b51adea57a6ccfa111546bdc9333
VM-Win10-OS2\russe <russell.clark@when-red.com>
2018-02-10 12:13:01 +0000

    Added new ilinks to page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('PDTemplateANOS5 - Firebase Hosted');
+    expect(compiled.querySelector('h1').textContent).toContain('PDTemplateANOS5 - Hosted on: Firebase and GitHub Pages');
   }));
 });
